a4b7f3e6355d05b338bd3566e8d0508f92794f52
FT1ORB <rstoyo3.furumoto.hidemasa@techcamp-training.com>
2024-05-07 17:01:58 +0900

    posts#indexへのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'questions#index'
-  resources :questions, only: [:index, :show, :new, :create] do
-    resources :answers, only: :create
-  end
- end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'posts#index'
+end
